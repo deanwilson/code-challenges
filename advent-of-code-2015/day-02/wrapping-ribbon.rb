bd958ec60bf2f1ae98c1cb92be04768c289a342f
Dean Wilson <dean.wilson@gmail.com>
2023-12-20 19:29:32 +0000

    Simplify the to_i calls a little

--- advent-of-code-2015/day-02/wrapping-ribbon.rb
+++ advent-of-code-2015/day-02/wrapping-ribbon.rb
@@ -3,9 +3,9 @@ presents = File.read('input').split.map { |m| m.chomp }
 total_size = 0
 
 presents.each do |present|
-  length, width, height = present.split('x').map { |m| m.to_i }
+  length, width, height = present.split('x').map(&:to_i)
 
-  a, b = present.split('x').map { |m| m.to_i }.sort[0,2]
+  a, b = present.split('x').map(&:to_i).sort[0,2]
 
   ribbon = a + a + b + b # you need to wrap each of the sides twice - front and back
   bow = length * width * height
